67a61218defb9a54790082c14f149c0ebb3ee229
alisajadih <alisajadih@gmail.com>
2020-06-08 00:43:23 +0430

    wtf homepage

--- app/components/HeaderNav.tsx
+++ app/components/HeaderNav.tsx
@@ -1,15 +1,21 @@
 import React from "react";
-import { useSelector } from "react-redux";
-import { IStore } from "../../pannels/src/redux/models/IStore";
 
-const HeaderNav = () => {
-  const user = useSelector((state: IStore) => state.user);
-  // console.log(user, "in header nav");
-  return (
-    <div>
-      <h1> This is navbar and use is : {user.client.first_name}</h1>
-    </div>
-  );
+export const HeaderNav = () => {
+  return <div></div>;
 };
 
-export default HeaderNav;
+// import React from "react";
+// import { useSelector } from "react-redux";
+// import { IStore } from "../../pannels/src/redux/models/IStore";
+
+// const HeaderNav = () => {
+//   const user = useSelector((state: IStore) => state.user);
+//   // console.log(user, "in header nav");
+//   return (
+//     <div>
+//       <h1> This is navbar and use is : {user.client.first_name}</h1>
+//     </div>
+//   );
+// };
+
+// export default HeaderNav;
--- app/pages/_app.tsx
+++ app/pages/_app.tsx
@@ -2,9 +2,29 @@ import withRedux from "next-redux-wrapper";
 import { Provider } from "react-redux";
 import { wrapper } from "../redux";
 import { AppProps } from "next/app";
+import "../assets/css/all-rtl.min.css";
+// import "../assets/css/ionicons-rtl.min.css";
+// import "../assets/css/themify-icons-rtl.css";
+// import "../assets/css/linearicons-rtl.css";
+// import "../assets/css/flaticon-rtl.css";
+// import "../assets/css/simple-line-icons-rtl.css";
+import "../assets/css/magnific-popup-rtl.css";
+import "../assets/css/slick-rtl.css";
+import "../assets/css/slick-theme-rtl.css";
+// import "../assets/css/simple-line-icons-rtl.css";
+// import "../assets/css/simple-line-icons-rtl.css";
+import "../assets/css/style-rtl.css";
+import "../assets/css/responsive-rtl.css";
+
+{
+  /* <link rel="stylesheet" href="assets/owlcarousel/css/owl.carousel.min.css">
+<link rel="stylesheet" href="assets/owlcarousel/css/owl.theme.css">
+<link rel="stylesheet" href="assets/owlcarousel/css/owl.theme.default.min.css"> */
+}
 
 const MyApp: React.FC<AppProps> = ({ Component, pageProps, ...rest }) => {
   return <Component {...pageProps} />;
 };
 
-export default wrapper.withRedux(MyApp);
+export default MyApp;
+// export default wrapper.withRedux(MyApp);
