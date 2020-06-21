440b1a2f8fa63ba997700a9c400c30759aa1df1b
alisajadih <alisajadih@gmail.com>
2020-06-21 13:32:33 +0430

    role admin

--- pannels/src/services/contexts/UserContext/UserContext.ts
+++ pannels/src/services/contexts/UserContext/UserContext.ts
@@ -4,7 +4,7 @@ import { IUserState } from "./models";
 const initial: IUserState = {
   username: "ali",
   isAuth: true,
-  rule: "company",
+  rule: "admin",
 };
 
 export const userContext = createContext<IUserState>(initial);
